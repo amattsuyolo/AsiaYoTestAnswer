f44b5e542e1c21cc75d12e35679f41a2eb0eadf5
mattsu <mattsuyolo@gmail.com>
2024-12-20 12:18:07 +0800

    feat:Add OrderRequest for basic data validation

---
+++ laravel/laravel/app/Http/Requests/OrderRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|string',
+            'name' => 'required|string',
+            'address.city' => 'required|string',
+            'address.district' => 'required|string',
+            'address.street' => 'required|string',
+            'price' => 'required|numeric',
+            'currency' => 'required|string',
+        ];
+    }
+}
